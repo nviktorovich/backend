5802b5b6c5c2e581995f1410d17a5c48f7866fd2
Nikita Maslenko <nvmaslenko@gmail.com>
2023-10-16 21:06:42 +0500

    add workflow, add entities testing

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/NViktorovich/cryptobackend
 go 1.21.1
 
 require (
-	github.com/go-chi/chi/v5 v5.0.10 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.4.3 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.8.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/entities/crypto_test.go
@@ -0,0 +1,56 @@
+package entities
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewCrypto(t *testing.T) {
+	type args struct {
+		shortTitle string
+		cost       float64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Crypto
+		wantErr bool
+	}{
+		{
+			name:    "new crypto create successful",
+			args:    args{shortTitle: "ETH", cost: 1.22},
+			want:    &Crypto{ShortTitle: "ETH", Cost: 1.22},
+			wantErr: false,
+		},
+		{
+			name:    "new crypto create err",
+			args:    args{shortTitle: "ETH", cost: -1.22},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewCrypto(tt.args.shortTitle, tt.args.cost)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewCrypto() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewCrypto() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSetTitle(t *testing.T) {
+	now := time.Now()
+	cr := &Crypto{ShortTitle: "ETH", Cost: 1.22}
+	cr.SetTitle("Ethereum")
+	require.Equal(t, "Ethereum", cr.Title)
+	cr.SetTimeStamp(now)
+	require.Equal(t, now, cr.TimeStamp)
+}
