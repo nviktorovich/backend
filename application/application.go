1f5a696fb5b25fabd04230c459dc80ca7857d0e7
Nikita Maslenko <nvmaslenko@gmail.com>
2023-10-22 10:19:18 +0500

    remove not realised code

--- application/application.go
+++ application/application.go
@@ -1,13 +1,1 @@
 package application
-
-import (
-	"github.com/NViktorovich/cryptobackend/internal/cases"
-	"github.com/NViktorovich/cryptobackend/internal/port/server"
-)
-
-func StartApp() {
-	var client cases.Client
-	var storage cases.Storage
-	var service server.Service
-
-}
